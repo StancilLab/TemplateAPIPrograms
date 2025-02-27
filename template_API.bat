a8e1f8d2ae99bc70141c5df302cccb97ac143c37
StancilLab <stancilresearch@cmh.edu>
2025-02-27 08:27:33 -0600

    Delete template_API.bat

--- template_API.bat
+++
@@ -1,4 +0,0 @@
-@REM This simply calls the program to run. Change the 'cd' line to where R is located. Should look similar to what is there. 
-cd C:/Users/aeburns2/AppData/Local/Programs/R/R-4.3.3/bin/x64
-@REM This next line calls the program you want. Copy the path to the R program and update. Ensure path looks similar to the one below and uses \
-Rscript "C:\Users\aeburns2\OneDrive - Children's Mercy Kansas City\Data for PowerBI\NNRCT-ProgramsForPBI\APIPullScreenAuto.R"
